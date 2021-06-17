89bc240e81e4beaeac4d8c90a2c22ff04158d480
Mans <hmw9309@gmail.com>
2021-06-17 16:00:14 +0900

    Chapter1 End

---
+++ src/test/java/me/whiteship/demospringsecurityform/Account/AccountControllerTest.java
@@ -0,0 +1,145 @@
+package me.whiteship.demospringsecurityform.Account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired AccountService accountService;
+
+    @Test
+    public void index_anonymous() throws Exception{
+        mockMvc.perform(get("/").with(anonymous()))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void index_anonymous_annotation() throws Exception{
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void index_user() throws Exception{
+        mockMvc.perform(get("/").with(user("ktnet").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "ktnet", roles = "USER")
+    public void index_user_annotation() throws Exception{
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUser // 자주 쓰는 애노테이션 따로 만들어서 재사용
+    public void index_user_annotation_recycle() throws Exception{
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void admin_user() throws Exception{
+        mockMvc.perform(get("/admin").with(user("ktnet").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "ktnet", roles = "USER")
+    public void admin_user_annotation() throws Exception{
+        mockMvc.perform(get("/admin"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void admin_admin() throws Exception{
+        mockMvc.perform(get("/admin").with(user("ktnet").roles("ADMIN")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "ktnet", roles = "ADMIN")
+    public void admin_admin_annotation() throws Exception{
+        mockMvc.perform(get("/admin"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    public void login_success() throws Exception{
+        String username = "ktnet";
+        String password = "123";
+        Account user = createUser(username, password);
+
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login_success2() throws Exception{
+        String username = "ktnet";
+        String password = "123";
+        Account user = createUser(username, password);
+
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login_fail() throws Exception{
+        String username = "ktnet";
+        String password = "123";
+        Account user = createUser(username, password);
+
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password("12345"))
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser(String ktnet, String password) {
+        Account account = new Account();
+        account.setUsername(ktnet);
+        account.setPassword(password);
+        account.setRole("USER");
+        accountService.signUp(account);
+
+        return account;
+    }
+
+}
